4e47f6dfb685c62ed99bc20469e1a32e0915de06
Diogo Correia <diogocorreia@devuo.net>
2014-05-02 15:00:34 +0100

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  var baseDirectory = grunt.option('base-directory');
+
+  // File Patterns
+  // --------------------------------------------------------------------------
+  var extensions = ['.css', '.tpl.php'];
+  var filePatterns = [];
+
+  for (var i = 0; i < extensions.length; i++) {
+    filePatterns.push(baseDirectory + '/**/*' + extensions[i]);
+  }
+
+  // Task Configuration
+  // --------------------------------------------------------------------------
+  var tasks = {
+
+    // Watch file changes
+    // ------------------------------------------------------------------------
+    watch: {
+      options: {
+        livereload: true
+      },
+
+      files: {
+        files: filePatterns,
+        tasks: []
+      },
+
+    }
+
+  };
+
+  // Composite Tasks
+  // --------------------------------------------------------------------------
+
+  grunt.registerTask('default', 'watch');
+
+  // Finish configuring Grunt
+  // --------------------------------------------------------------------------
+
+  grunt.initConfig(tasks);
+
+};
